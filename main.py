75d245340f6ddcd3277dc70f1b2410beefc48173
JohnKenny <squareindigojohn@yahoo.ie>
2026-03-25 15:59:15 +0000

    initial setup

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
  # logging tests in python
+
+import logging
+
+logging.basicConfig(level = logging.DEBUG)
+
+def add(a: int, b: int):
+    logging.info(f"Se ha llamado a la funcion add con entradas {a}, {b}.")
+    return a + b
+
+print(add(10, 30))
