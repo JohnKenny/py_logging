bf57559655e89760262ed4f4174fceebdf3d59e0
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-09 20:55:03 +0100

    multi-output logger function added

--- custom_loggers.py
+++ custom_loggers.py
@@ -1,1 +1,28 @@
 # module for logging configs
+import logging
+
+def my_logger() -> logging.Logger:
+    """ Function defines a multi-output logger"""
+    logger = logging.getLogger("Mylogger")
+    logger.setLevel(level=logging.DEBUG)
+
+    # Adds file handler
+    file_logger = logging.FileHandler("log.out")
+    file_logger.setLevel(logging.DEBUG)
+
+    # Adds console handler
+    console_logger = logging.StreamHandler()
+    console_logger.setLevel(logging.INFO)
+
+    # Adds formatting
+    formatter = logging.Formatter(f'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_logger.setFormatter(formatter)
+    console_logger.setFormatter(formatter)
+
+    # Adds handlers to the logger
+    logger.addHandler(file_logger)
+    logger.addHandler(console_logger)
+
+    return logger
+
+logger = my_logger()
--- main.py
+++ main.py
@@ -1,33 +1,13 @@
- # logging tests in python, Log Handlers
  
-import logging
+from custom_loggers import logger
 
-logger = logging.getLogger("Mylogger")
-logger.setLevel(level=logging.DEBUG)
-
-# Adds file handler
-file_logger = logging.FileHandler("log.out")
-file_logger.setLevel(logging.DEBUG)
-
-# Adds console handler
-console_logger = logging.StreamHandler()
-console_logger.setLevel(logging.INFO)
-
-# Adds formatting
-formatter = logging.Formatter(f'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_logger.setFormatter(formatter)
-console_logger.setFormatter(formatter)
-
-# Adds handlers to the logger
-logger.addHandler(file_logger)
-logger.addHandler(console_logger)
+logger.info("This is an info message")
+logger.debug("This is a debug message")
 
 
-def myfunc():
-    logger.info("mensaje de informacion")
-    logger.debug("mensaje de debug")
 
-myfunc()
+
+
 
 
 
