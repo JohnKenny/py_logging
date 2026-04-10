e1b36ee848d20aebcaee70297bce8da410b3a881
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-10 21:16:50 +0100

    call successful

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import requests
 from custom_loggers import logger
 
+url = 'https://httpbin.org/get'
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print("Datos de respuesta:")
+    print(data["headers"])
+else:
+    print(f"La solicitud fallo con el codigo {response.status_code}")
+
+
 
 
 
