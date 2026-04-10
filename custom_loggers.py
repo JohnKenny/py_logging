c1d872e7c641baf97a4c46beec1c1b066cc4a26b
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-10 12:43:57 +0100

    separated concerns

--- custom_loggers.py
+++ custom_loggers.py
@@ -1,27 +1,30 @@
 # module for logging configs
 import logging
 
+my_formatter = logging.Formatter(f'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+def file_logger() -> logging.FileHandler:
+    """Defines and returns a file handler"""
+    file_logger = logging.FileHandler("log.out")
+    file_logger.setLevel(logging.DEBUG)
+    file_logger.setFormatter(my_formatter)
+    
+    return file_logger
+
+def console_logger() -> logging.StreamHandler:
+    """Defines and returns a stream handler"""
+    console_logger = logging.StreamHandler()
+    console_logger.setLevel(logging.INFO)
+    console_logger.setFormatter(my_formatter)
+    
+    return console_logger
+
 def my_logger() -> logging.Logger:
     """ Function defines a multi-output logger"""
     logger = logging.getLogger("Mylogger")
     logger.setLevel(level=logging.DEBUG)
-
-    # Adds file handler
-    file_logger = logging.FileHandler("log.out")
-    file_logger.setLevel(logging.DEBUG)
-
-    # Adds console handler
-    console_logger = logging.StreamHandler()
-    console_logger.setLevel(logging.INFO)
-
-    # Adds formatting
-    formatter = logging.Formatter(f'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_logger.setFormatter(formatter)
-    console_logger.setFormatter(formatter)
-
-    # Adds handlers to the logger
-    logger.addHandler(file_logger)
-    logger.addHandler(console_logger)
+    logger.addHandler(file_logger())
+    logger.addHandler(console_logger())
 
     return logger
 
