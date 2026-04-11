0f73b9f6e494b1e6b6dda71b0b26a589c3ec58f5
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-11 10:57:57 +0100

    logging added

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from custom_loggers import logger
 
 url = 'https://httpbin.org/get'
 response = requests.get(url)
+logger.info("Requesting from: %s", url)
 
 if response.status_code == 200:
     data = response.json()
-    print("Datos de respuesta:")
-    print(data["headers"])
+    logger.info("Headers %s ", data["headers"])
 else:
     print(f"La solicitud fallo con el codigo {response.status_code}")
 
