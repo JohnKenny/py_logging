9df6b4a4b0e7e58728c63f8cb548b60a46ac387a
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-11 21:28:08 +0100

    rotating logger start

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import requests
-from custom_loggers import logger
 
-url = 'https://httpbin.org/get'
-
-try:
-    response = requests.get(url)
-    logger.info("Requesting from: %s", url)
-except Exception as exc:
-    logger.critical("Ocurrio un error critico.. %s", exc)
-else:
-    if response.status_code == 200:
-        data = response.json()
-        logger.info("Headers %s ", data["headers"])
-    else:
-        logger.error("Request failed with code: %s", response.status_code)
 
 
 
