b6833e2107c4a7334b3cfb69d2b5b78a54e7a939
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-11 12:15:10 +0100

    added error handling

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import requests
 from custom_loggers import logger
 
 url = 'https://httpbin.org/get'
-response = requests.get(url)
-logger.info("Requesting from: %s", url)
 
-if response.status_code == 200:
-    data = response.json()
-    logger.info("Headers %s ", data["headers"])
+try:
+    response = requests.get(url)
+    logger.info("Requesting from: %s", url)
+except Exception as exc:
+    logger.critical("Ocurrio un error critico.. %s", exc)
 else:
-    print(f"La solicitud fallo con el codigo {response.status_code}")
+    if response.status_code == 200:
+        data = response.json()
+        logger.info("Headers %s ", data["headers"])
+    else:
+        logger.error("Request failed with code: %s", response.status_code)
 
 
 
