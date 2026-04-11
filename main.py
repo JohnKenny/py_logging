369b628555e2531fab6ab2f164802aa4153cf207
JohnKenny <squareindigojohn@yahoo.ie>
2026-04-11 21:50:24 +0100

    rotating handler end

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import logging
+import time
+from logging.handlers import RotatingFileHandler
+
+handler = RotatingFileHandler(filename="log.out", 
+                              maxBytes=2048, 
+                              backupCount=4)
+
+logger = logging.getLogger("MyLogger")
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+for _ in range(1000):
+    statement = f"El momento es ahora {str(time.time())}"
+    logger.debug(statement)
+
 
 
 
